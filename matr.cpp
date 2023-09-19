ab5bf0da0d8f3370c4fe77be31dddb963ac3290c
Школа программирования 1 08 <schpr1-08@int.adtspb.ru>
2023-09-19 19:46:38 +0300

    version 1

---
+++ matr.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main() {
+
+	using namespace std;
+
+	int m = 0;
+	int n = 0;
+	cout << "введите размеры матрицы :\n";
+	cin >> n >> m;
+	int mas[100][100];
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> mas[i][j];
+		}
+	}
+
+
+}
