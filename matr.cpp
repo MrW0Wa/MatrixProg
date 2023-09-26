1b47b000c25a0edb859b768f67098001e3c629d1
MrW0Wa <vovaxme@gmail.com>
2023-09-26 16:37:05 +0300

    26.09.23

--- matr.cpp
+++ matr.cpp
@@ -1,20 +1,65 @@
 ﻿#include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
+    int r1, c1, r2, c2;
 
-	using namespace std;
+    cout << "количество строк и столбцов первой матрицы: ";
+    cin >> r1 >> c1;
 
-	int m = 0;
-	int n = 0;
-	cout << "введите размеры матрицы :\n";
-	cin >> n >> m;
-	int mas[100][100];
+    cout << "количество строк и столбцов второй матрицы: ";
+    cin >> r2 >> c2;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> mas[i][j];
-		}
-	}
+    vector<vector<int>> matrix1(r1, vector<int>(c1));
+    vector<vector<int>> matrix2(r2, vector<int>(c2));
+    vector<vector<int>> result(r2, vector<int>(c2));
 
+    cout << "Введите элементы первой матрицы:" << endl;
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c1; ++j) {
+            cin >> matrix1[i][j];
+        }
+    }
 
+    cout << "Введите элементы второй матрицы:" << endl;
+    for (int i = 0; i < r2; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            cin >> matrix2[i][j];
+        }
+    }
+
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c1; ++j) {
+            for (int n = 0; n < r1; ++n) {
+                result[i][j] += matrix1[i][n] * matrix2[n][j];
+            }
+        }
+    }
+
+    cout << "Первая матрица:" << endl;
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c1; ++j) {
+            cout << matrix1[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "Вторая матрица:" << endl;
+    for (int i = 0; i < r2; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            cout << matrix2[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "умножение" << endl;
+    for (int i = 0; i < r2; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cin >> r2;
 }
